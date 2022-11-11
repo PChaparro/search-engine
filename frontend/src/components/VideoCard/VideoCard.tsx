01c99f0dc73af686c36a634a9132f87215e65297
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-11 16:26:59 -0500

    :sparkles: Show videos after back-end request

---
+++ frontend/src/components/VideoCard/VideoCard.tsx
@@ -0,0 +1,31 @@
+import Styles from './VideoCard.module.css';
+import { IVideo } from '../../interfaces/videos';
+
+interface IProps {
+  video: Ivideo;
+}
+
+export const VideoCard = ({ video }: IProps) => {
+  return (
+    <article className={Styles.card}>
+      <a href={video.url} target='_blank' rel='noopener'>
+        <img src={video.thumbnail} alt={video.title} className={Styles.card__image} />
+      </a>
+      <div className={Styles.card__body}>
+        <a href={video.url} target='_blank' rel='noopener'>
+          <h3 className={Styles.card__title}>{video.title}</h3>
+        </a>
+        <div className={Styles.card__tags}>
+          {video.tags.length !== 0 &&
+            video.tags.slice(0, 3).map((tag, index) => {
+              return (
+                <span className={Styles.card__tag} key={`${video.url}-${index}`}>
+                  {tag}
+                </span>
+              );
+            })}
+        </div>
+      </div>
+    </article>
+  );
+};
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Styles from './Home.module.css';
 
 import { IVideo } from '../../interfaces/videos';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
+import { VideoCard } from '../../components/VideoCard/VideoCard';
 import { useGetVideos } from '../../hooks/useGetVideos/useGetVideos';
 
 export const Home = () => {
@@ -11,7 +12,9 @@ export const Home = () => {
     <main className={Styles.container}>
       <SearchBar searchCallback={search} />
       <section className={Styles.videos}>
-        <p>{videos.length}</p>
+        {videos.map((video, index) => {
+          return <VideoCard video={video} key={video.url} />;
+        })}
       </section>
     </main>
   );
