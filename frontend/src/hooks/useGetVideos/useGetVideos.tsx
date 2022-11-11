d356339771f16ae53013a90872d5e3ce95f9ee8f
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-11 18:00:22 -0500

    :lipstick: Add loader
    Loader while fetch get a response

--- frontend/src/hooks/useGetVideos/useGetVideos.tsx
+++ frontend/src/hooks/useGetVideos/useGetVideos.tsx
@@ -4,14 +4,18 @@ import { SearchVideosService } from '../../services/videos.services';
 
 export const useGetVideos = () => {
   const [videos, setVideos] = useState(Array<IVideo>);
+  const [isLoading, setIsLoading] = useState(false);
 
   const search = async (criteria: string) => {
+    setIsLoading(true);
     const videos = await SearchVideosService(criteria);
     setVideos(videos);
+    setIsLoading(false);
   };
 
   return {
     videos,
     search,
+    isLoading,
   };
 };
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,20 +1,22 @@
 import Styles from './Home.module.css';
 
-import { IVideo } from '../../interfaces/videos';
+import { BallTriangle } from '@agney/react-loading';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
 import { VideoCard } from '../../components/VideoCard/VideoCard';
 import { useGetVideos } from '../../hooks/useGetVideos/useGetVideos';
 
 export const Home = () => {
-  const { videos, search } = useGetVideos();
+  const { videos, search, isLoading } = useGetVideos();
 
   return (
     <main className={Styles.container}>
       <SearchBar searchCallback={search} />
+      {isLoading && <div className={Styles.loader}><BallTriangle color='#4766da' width={72} /></div>}
       <section className={Styles.videos}>
-        {videos.map((video, index) => {
-          return <VideoCard video={video} key={video.url} />;
-        })}
+        {!isLoading &&
+          videos.map((video, index) => {
+            return <VideoCard video={video} key={video.url} />;
+          })}
       </section>
     </main>
   );
