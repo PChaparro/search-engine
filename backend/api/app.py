473f1271e83b2fcfd14fa2e6ab3a1ceaf835d1c4
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-11 20:21:55 -0500

    :ambulance: Allow cors

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,6 +1,18 @@
-from bottle import run
+from bottle import run, hook, response
+
+_allow_origin = "*"
+_allow_methods = "GET, POST, OPTIONS"
+_allow_headers = "Origin, Accept, Content-Type, X-Requested-With"
 
 # Setup routes
 from controllers import controllers
+
+# Allow cors
+@hook("after_request")
+def enable_cors():
+    response.headers["Access-Control-Allow-Origin"] = _allow_origin
+    response.headers["Access-Control-Allow-Methods"] = _allow_methods
+    response.headers["Access-Control-Allow-Headers"] = _allow_headers
+
 
 run(host="localhost", port=8080, debug=True, reloader=True)
